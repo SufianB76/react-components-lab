fac8e8550ed4f89f034a03610d0f316873e8a6e0
Sufian <sufianbhatti76@gmail.com>
2024-12-03 20:54:16 -0500

    Homework GA

---
+++ src/WeatherForecast.jsx
@@ -0,0 +1,20 @@
+import './WeatherForecast.css';
+
+function weatherForecast({ forecast }) {
+  return (
+    <div className="weather">
+      <h2>{forecast.day}</h2>
+      <img src={forecast.img} alt={forecast.imgAlt} />
+      <p>
+        <span>Conditions: </span>
+        {forecast.conditions}
+      </p>
+      <p>
+        <span>Time: </span>
+        {forecast.time}
+      </p>
+    </div>
+  );
+}
+
+export default weatherForecast;
